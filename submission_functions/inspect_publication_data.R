16e8b3fd668a9ab24016e430c1bb3cf7053b9bbc
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-05-06 16:15:57 +0200

    move to acdc db

--- submission_functions/inspect_publication_data.R
+++
@@ -1,5 +0,0 @@
-inspect_publication_data <- function(submission_obj){
-  publication_data = submission_obj$publication_data
-  
-  return(publication_data)
-}
--- submission_functions/prep_measurement_data.R
+++
@@ -1,4 +0,0 @@
-prep_measurement_data <- function(measurement_data, db_overview){
-  measurement_data <- clean_char_columns(measurement_data, db_overview, "measure_table")
-  return(measurement_data)
-}
