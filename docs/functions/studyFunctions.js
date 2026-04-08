b61afa018982331d36a298e478b19304c5858dd1
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2026-04-08 10:50:03 +0200

    study updated

--- docs/functions/studyFunctions.js
+++ docs/functions/studyFunctions.js
@@ -85,23 +85,33 @@ function initializeStudySurvey(control, publication_idx, study_idx) {
 
     document.getElementById("content").innerHTML = `
     <div class="display-text">
-        <h1>${study_name}</h1>
-        <p>This section is designed to collect key details about the study you conducted. The information you provide here will help us better understand the scope and methodology of your research. You'll be asked about how you measured truth ratings, what other measures were included in your study, the types of analysis you performed, and the specific statements or stimuli used in your research.</p>
-        <p>If your study is available on an open-source platform, you’ll also have the option to share a link, ensuring that others can access the full study for further exploration.</p>
-        <p>This information is crucial for ensuring that your study is well-documented and can be effectively integrated into our database. Thank you for taking the time to provide these details.</p>
-        <p>You will be asked to enter information from different between-subject conditions in different "dataset" surveys. Make sure that the number of between-subject conditions in the dataset survey matches the number of between-subject conditions you enter here.</p>
+        <h1 class = "mb-3">${study_name}</h1>
+        <div class="alert alert-info" role="alert">
+            <h5 class="alert-heading"><i class="bi bi-info-circle me-2"></i>Before You Begin</h5>
+            <p>This section is designed to collect key details about the study you conducted. The information you provide here will help us better understand the scope and methodology of your research. You'll be asked about how you measured truth ratings, what other measures were included in your study, the types of analysis you performed, and the specific statements or stimuli used in your research.</p>
+            <p>If your study is available on an open-source platform, you’ll also have the option to share a link, ensuring that others can access the full study for further exploration.</p>
+            <p>This information is crucial for ensuring that your study is well-documented and can be effectively integrated into our database. Thank you for taking the time to provide these details.</p>
+            <p>You will be asked to enter information from different between-subject conditions in different "dataset" surveys. Make sure that the number of between-subject conditions in the dataset survey matches the number of between-subject conditions you enter here.</p>
+        </div>
 
-        <form id="studySurvey" class="survey-form">
-            <label for="n_groups" class = "survey-label">How many between-subject groups are present in this study?</label>
-            <input type="number" id="n_groups" name="n_groups" value="${study_data.n_groups || ''}"><br>
+        <h3 class="mb-3">Study Survey</h2>
+        <form id="studySurvey" class="survey-form p-3 border rounded shadow-sm bg-light">
+            <div class="mb-3">
+            <label for="n_groups" class = "form-label">How many between-subject groups are present in this study?</label>
+            <input type="number" class="form-control" id="n_groups" name="n_groups" value="${study_data.n_groups || ''}">
+            </div>
             
-            <label for="n_tasks" class = "survey-label">How many tasks did you use in this study?</label>
-            <input type="number" id="n_tasks" name="n_tasks" value="${study_data.n_tasks || ''}"><br>
+            <div class="mb-3">
+            <label for="n_tasks" class = "form-label">How many tasks did you use in this study?</label>
+            <input type="number" class="form-control" id="n_tasks" name="n_tasks" value="${study_data.n_tasks || ''}">
+            </div>
 
-            <label for="study_comment" class = "survey-label">Please provide a short description of the study (1-2 sentences): What was investigated? How was it studied?</label>
-            <input type="text" id="study_comment" name="study_comment" value="${study_data.study_comment || ''}"><br>
+            <div class="mb-3">
+            <label for="study_comment" class = "form-label">Please provide a short description of the study (1-2 sentences): What was investigated? How was it studied?</label>
+            <input type="text" class="form-control" id="study_comment" name="study_comment" value="${study_data.study_comment || ''}">
+            </div>
 
-            <button type="submit" class="survey-button">Submit</button>
+            <button type="submit" class="btn btn-success">Submit</button>
         </form>
     </div>
     `;
@@ -154,7 +164,7 @@ function updateStudySurvey(control, publication_idx, study_idx) {
     control.publication_info[publication_idx].study_info[study_idx].study_data = study_data;
 
     // Optionally, display a confirmation message
-    alert('Survey submitted successfully!');
+    showAlert('Survey submitted successfully!', 'success');
 
     // Add a checkmark to the currently selected sidebar item
     const item_id =  "study-" + publication_idx + "-" + study_idx;
